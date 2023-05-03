d4990d3268cbe074d37bf4e740b4d252cd31e9a6
Theodim <theodim132@gmail.com>
2023-05-04 00:23:34 +0300

    azure service bus api

--- ServiceBusTrigger/ServiceBusTrigger/Function1.cs
+++ ServiceBusTrigger/ServiceBusTrigger/Function1.cs
@@ -8,7 +8,7 @@ namespace ServiceBusTrigger
     public class Function1
     {
         [FunctionName("Function1")]
-        public void Run([ServiceBusTrigger("jdlearningqueue", Connection = "ServiceBusConnectionString")]string myQueueItem, ILogger log)
+        public void Run([ServiceBusTrigger("create", Connection = "Endpoint=sb://my-argo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Qrokrlyxg/QF0qLjnX3TUbhOS3UG3tE5Q+ASbGjOh54=")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
         }
